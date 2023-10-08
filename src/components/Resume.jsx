5717b1e4461928be8902aee2d4c832c02f78af0e
Hamby <hannah1bae@gmail.com>
2023-10-08 02:19:53 -0700

    setup resume formatting

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -11,18 +11,26 @@ function Resume() {
             <p>habae1004@gmail.com</p>
           </div>
         </div>
-        <p className="summary main-part section-content">
+        <p className="summary main-part section-content-indent">
           Self-motivated IT professional with three years of experience and a B.S. in human-computer interaction. Key strengths include communication, analytical problem-solving, attention to detail, and eagerness to learn new skills and tools.
         </p>
         <div className="education-section side-part">
-          <h5 className="section-title">EDUCATION</h5>
+          <div className="education-content">
+            <h5 className="section-title">EDUCATION</h5>
+            {/* insert content here */}
+          </div>
         </div>
         <div className="experience-section main-part">
-          <h5 className="section-title">EXPERIENCE</h5>
+          <div className="experience-content">
+            <h5 className="section-title">EXPERIENCE</h5>
+            {/* insert section-content here */}
+          </div>
         </div>
-        <div className="skills-section side-part">
-          <h5 className="section-title">SKILLS</h5>
-        </div>
+        <div className="skills-sectionside-part">
+          <div className="skills-content">
+            <h5 className="section-title">SKILLS</h5></div>
+            {/* insert section-content */}
+          </div>
       </div>
     </section>
   )
