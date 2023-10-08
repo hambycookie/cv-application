b29704c0c9b9dd042fbbf1e283d9d0d373cdbdf6
Hamby <hannah1bae@gmail.com>
2023-10-08 16:52:25 -0700

    add accordions!

--- src/components/FormSection.jsx
+++ src/components/FormSection.jsx
@@ -1,15 +1,17 @@
 import '../styles/FormSection.css'
+import Collapsible from 'react-collapsible'
 
 function FormSection({ formClassName, formTitle, children }) {
-
-
   return (
-    <section className="form-card">
+    <Collapsible 
+      className="form-card"
+      trigger={<h5 className="form-title">{formTitle}</h5>}
+      transitionTime={150}
+    >
       <form className={formClassName}>
-        <h5 className="form-title">{formTitle}</h5>
         {children}
       </form>
-    </section>
+    </Collapsible>
   )
 }
 
