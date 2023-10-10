de3f5f1bd073a55b29411d5d6482058220f570fb
Hamby <hannah1bae@gmail.com>
2023-10-10 01:13:16 -0700

    make resume responsive

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -5,34 +5,43 @@ import ExperienceSection from "./Experience/ExperienceSection"
 function Resume({ personalInfo, summary, educations, experiences }) {
   return (
     <section className="resume-paper">
-      <div className="resume">
-        <h1 className="name main-part">{personalInfo.fullName}</h1>
-        <div className="contact-section side-part">
-          <div className="contact-info">
-            <p>{personalInfo.phone}</p>
-            <p>{personalInfo.email}</p>
+      <div className="resume-container">
+        <div className="main-part">
+          <div className="name-section">
+            <h1 className="name">{personalInfo.fullName}</h1>
+          </div>
+          <div className="summary-section">
+            <p className="summary">
+              {summary}
+            </p>
+          </div>
+          <div className="experience-section">
+            <h5 className="section-title">EXPERIENCE</h5>
+            <div className="experience-content">
+              <ExperienceSection experiences={experiences} />
+            </div>
           </div>
         </div>
-        <p className="summary main-part section-content-indent">
-          {summary}
-        </p>
-        <div className="education-section side-part">
-          <h5 className="section-title">EDUCATION</h5>
-          <div className="education-content">
-            <EducationSection educations={educations} />
+        <div className="side-part">
+          <div className="contact-section">
+            <div className="contact-info">
+              <p>{personalInfo.phone}</p>
+              <p>{personalInfo.email}</p>
+            </div>
+          </div>
+          <div className="education-section">
+            <h5 className="section-title">EDUCATION</h5>
+            <div className="education-content">
+              <EducationSection educations={educations} />
+            </div>
+          </div>
+          <div className="skills-section">
+            <h5 className="section-title">SKILLS</h5>
+            <div className="skills-content">
+              {/* insert section-content */}
+            </div>
           </div>
         </div>
-        <div className="experience-section main-part">
-          <h5 className="section-title">EXPERIENCE</h5>
-          <div className="experience-content">
-            <ExperienceSection experiences={experiences}/>
-          </div>
-        </div>
-        <div className="skills-section side-part">
-          <h5 className="section-title">SKILLS</h5></div>
-          <div className="skills-content">
-            {/* insert section-content */}
-          </div>
       </div>
     </section>
   )
